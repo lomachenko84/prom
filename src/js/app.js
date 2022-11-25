eb58689c209f0c83693a752b2e73212ad5159288
Lomachenko84 <agent84@inbox.ru>
2022-11-26 00:04:27 +0300

    prom.app

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,45 @@
-// TODO: write your code here
-import sum from './basic';
 
-console.log('worked');
+function json(data) {
+    return new Promise((resolve, reject) => {
+        // эмуляция обработки ArrayBuffer
+        setTimeout(() => {
+            resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
+        }, 500);
+    });
+}
+function read() {
+    return new Promise((resolve, reject) => {
+        // эмуляция чтения файла
+        setTimeout(() => {
+            const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+            return (input => {
+                const buffer = new ArrayBuffer(input.length * 2);
+                const bufferView = new Uint16Array(buffer);
+                for (let i = 0; i < input.length; i++) {
+                    bufferView[i] = input.charCodeAt(i);
+                }
+                resolve(buffer);
+            })(data);
+        }, 1000);
+    });
+}
+class GameSavingLoader {
+    static load() {
+        return new Promise((resolve, reject) => {
+            let data;
+            read().then((buffer) => {
+                data = buffer
+                json(data).then((son) => {
+                        resolve(son);
 
-console.log(sum([1, 2]));
+                })
+
+            });
+        })
+
+
+    }
+}
+GameSavingLoader.load().then((saving) => {
+    console.log(saving)
+})
