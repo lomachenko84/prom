c2cc11e629fc2857206f4e9e1129f57c6bbf5e18
Lomachenko84 <agent84@inbox.ru>
2022-11-26 20:37:30 +0300

    prom.test

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,6 @@
+import { json, read } from "./basic";
 
-function json(data) {
-    return new Promise((resolve, reject) => {
-        // эмуляция обработки ArrayBuffer
-        setTimeout(() => {
-            resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-        }, 500);
-    });
-}
-function read() {
-    return new Promise((resolve, reject) => {
-        // эмуляция чтения файла
-        setTimeout(() => {
-            const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
-            return (input => {
-                const buffer = new ArrayBuffer(input.length * 2);
-                const bufferView = new Uint16Array(buffer);
-                for (let i = 0; i < input.length; i++) {
-                    bufferView[i] = input.charCodeAt(i);
-                }
-                resolve(buffer);
-            })(data);
-        }, 1000);
-    });
-}
-class GameSavingLoader {
+export class GameSavingLoader {
     static load() {
         return new Promise((resolve, reject) => {
             let data;
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,24 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
-  }
-  return result;
+export function json(data) {
+  return new Promise((resolve, reject) => {
+      // эмуляция обработки ArrayBuffer
+      setTimeout(() => {
+          resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
+      }, 500);
+  });
+}
+export function read() {
+  return new Promise((resolve, reject) => {
+      // эмуляция чтения файла
+      setTimeout(() => {
+          const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+          return (input => {
+              const buffer = new ArrayBuffer(input.length * 2);
+              const bufferView = new Uint16Array(buffer);
+              for (let i = 0; i < input.length; i++) {
+                  bufferView[i] = input.charCodeAt(i);
+              }
+              resolve(buffer);
+          })(data);
+      }, 1000);
+  });
 }
