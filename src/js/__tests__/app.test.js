04ce776d7509a2b265a1f07e979c006090288202
Lomachenko84 <agent84@inbox.ru>
2022-12-14 22:18:30 +0300

    prom 2.1.1

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,7 @@
 import { GameSavingLoader } from "../app";
 
-test("test profiGame", async () => {
-    const data = await GameSavingLoader.load();
+test("test profiGame", () => {
+ return GameSavingLoader.load().then(data => {
     expect(data).toEqual({"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}});
-   
+ });  
 })
