7813338a6a9e5ae1ba96520831450d9af6d884a3
Lomachenko84 <agent84@inbox.ru>
2022-12-16 23:29:50 +0300

    prom 2.1.1.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,21 @@
 import { json, read } from "./basic";
+class GameSaving {
+    constructor(data) {
+        this.id = data.id
+        this.created = data.created
+        this.userInfo = {
+            id: data.userInfo.id, // user id
+            name: data.userInfo.name, // user name
+            level: data.userInfo.level, // user level
+            points: data.userInfo.points, // user points
+        }
+
+    }
+}
 
 export class GameSavingLoader {
     static load() {
-        return read().then((buffer) => json(buffer).then((son) => JSON.parse(son)));
+        return read().then((buffer) => json(buffer).then((son) => new GameSaving(JSON.parse(son))))
     }
 
 
